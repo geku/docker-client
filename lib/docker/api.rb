a4564a62873bb52b9c172b13d7f5a80db6de5ffe
Georg Kunz <kwd@gmx.ch>
2013-05-24 12:21:32 +0200

    Pass correct connection to resources

--- lib/docker/api.rb
+++ lib/docker/api.rb
@@ -8,20 +8,20 @@ class Docker::API
   attr_reader :connection
   
   def initialize(options)
-    @debug           = options[:debug]
-    @ssl             = options[:ssl] || { :verify => false }
-    base_url         = options[:base_url]
+    @debug      = options[:debug]
+    @ssl        = options[:ssl] || { :verify => false }
+    base_url    = options[:base_url]
     @connection = Docker::Connection.new(base_url: base_url)
     # @faraday_adapter = options[:faraday_adapter] || Faraday.default_adapter
     # @faraday         = options[:faraday]         || default_faraday
   end
   
   def containers
-    Docker::Resource::Container.new(@conn)
+    Docker::Resource::Container.new(@connection)
   end
   
   def images
-    Docker::Resource::Image.new(@conn)
+    Docker::Resource::Image.new(@connection)
   end
   
   def system
